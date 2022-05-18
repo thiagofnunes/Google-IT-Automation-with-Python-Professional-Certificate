7b61682c0bb5bcfba55575935c4b9876c930e3ab
ahm22004 <94788163+ahm22004@users.noreply.github.com>
2022-05-18 21:09:51 +0200

    simplifing the code
    combine the variable and the function of the script to reduce steps

--- findJane.sh
+++ findJane.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-grep " jane " ../data/list.txt | cut -d ' ' -f 3 > tempFile.txt
-files=$(<tempFile.txt)
+ 
+files=$(grep " jane " ../data/list.txt | cut -d ' ' -f 3)
 for i in $files ; do
- if test -e "..$i"; then echo "$i" >> oldFiles.txt;
+ if test -e "..$i"; then echo "$i" > oldFiles.txt;
  fi
 done
